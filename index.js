02f752e31b9e118a221f376e352fd040269fb122
fouad ajayi <fouadajayi6@gmail.com>
2022-10-29 13:50:01 +0100

    Made changes

--- index.js
+++ index.js
@@ -36,26 +36,36 @@ overlay.addEventListener('click', function(){
     overlay.classList.add('hidden');
 })
 
-YOU CAN ALWAYS USE THE TWO METHOD
-BUT I PREFER  TO USE THIS >>>
+// YOU CAN ALWAYS USE THE TWO METHOD
+// BUT I PREFER  TO USE THIS >>>
 
-let modal = document.querySelector('.modal');
-let overlay = document.querySelector('.overlay')
-let closebtn = document.querySelector('.close-modal')
-let openbtn = document.querySelector('.show-modal')
+let modals = document.querySelector('.modal');
+let overlays = document.querySelector('.overlay')
+let closebtns = document.querySelector('.close-modal')
+let openbtns = document.querySelector('.show-modal')
 
 
-let openModal = function(){
+let openModals = function(){
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 }
 
-let closemodal = function(){
+let closemodals = function(){
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 }
 
 
-openbtn.addEventListener('click' , openModal)
-overlay.addEventListener('click' , closemodal)
-closebtn.addEventListener('click' , closemodal)
+openbtns.addEventListener('click' , openModal)
+overlays.addEventListener('click' , closemodal)
+closebtns.addEventListener('click' , closemodal)
+
+//USING THE ESCAPE KEY TO CLOSE THE MODAL
+document.addEventListener('keydown' , function(e){
+    console.log(e.key)
+
+    if(e.key === 'Escape' &&  !modal.classList.contains('hidden')){
+            closemodal();
+        }
+    }
+)
