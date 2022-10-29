508e9fd591959ada32a6198428febc462dfe2f3c
fouad ajayi <fouadajayi6@gmail.com>
2022-10-29 09:09:33 +0100

    Made new changes

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 let modal = document.querySelector('.modal');
 let overlay = document.querySelector('.overlay')
 let closebtn = document.querySelector('.close-modal')
-
 let openbtn = document.querySelector('.show-modal')
 
 
-openbtn.addEventListener('click', function(){
+let openModal = function(){
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
-})
+}
+
+let closemodal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+
+openbtn.addEventListener('click' , openModal)
+overlay.addEventListener('click' , closemodal)
+closebtn.addEventListener('click' , closemodal)
