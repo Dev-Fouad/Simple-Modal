1c20d3b5f6aa315a1bd00892f71e4ff9388427e1
fouad ajayi <fouadajayi6@gmail.com>
2022-10-29 11:55:37 +0100

    MODAL WIND0W

--- index.js
+++ index.js
@@ -36,26 +36,26 @@ overlay.addEventListener('click', function(){
     overlay.classList.add('hidden');
 })
 
-//YOU CAN ALWAYS USE THE TWO METHOD
-// BUT I PREFER  TO USE THIS >>>
+YOU CAN ALWAYS USE THE TWO METHOD
+BUT I PREFER  TO USE THIS >>>
 
-// let modal = document.querySelector('.modal');
-// let overlay = document.querySelector('.overlay')
-// let closebtn = document.querySelector('.close-modal')
-// let openbtn = document.querySelector('.show-modal')
-
-
-// let openModal = function(){
-//     modal.classList.remove('hidden');
-//     overlay.classList.remove('hidden');
-// }
-
-// let closemodal = function(){
-//     modal.classList.add('hidden');
-//     overlay.classList.add('hidden');
-// }
+let modal = document.querySelector('.modal');
+let overlay = document.querySelector('.overlay')
+let closebtn = document.querySelector('.close-modal')
+let openbtn = document.querySelector('.show-modal')
 
 
-// openbtn.addEventListener('click' , openModal)
-// overlay.addEventListener('click' , closemodal)
-// closebtn.addEventListener('click' , closemodal)
+let openModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+let closemodal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+
+openbtn.addEventListener('click' , openModal)
+overlay.addEventListener('click' , closemodal)
+closebtn.addEventListener('click' , closemodal)
