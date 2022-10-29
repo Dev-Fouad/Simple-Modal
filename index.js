a12db593c9f3edcaf8a78383b3381120039e8939
fouad ajayi <fouadajayi6@gmail.com>
2022-10-29 11:52:40 +0100

    A modal window

--- index.js
+++ index.js
@@ -35,3 +35,27 @@ overlay.addEventListener('click', function(){
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 })
+
+//YOU CAN ALWAYS USE THE TWO METHOD
+// BUT I PREFER  TO USE THIS >>>
+
+// let modal = document.querySelector('.modal');
+// let overlay = document.querySelector('.overlay')
+// let closebtn = document.querySelector('.close-modal')
+// let openbtn = document.querySelector('.show-modal')
+
+
+// let openModal = function(){
+//     modal.classList.remove('hidden');
+//     overlay.classList.remove('hidden');
+// }
+
+// let closemodal = function(){
+//     modal.classList.add('hidden');
+//     overlay.classList.add('hidden');
+// }
+
+
+// openbtn.addEventListener('click' , openModal)
+// overlay.addEventListener('click' , closemodal)
+// closebtn.addEventListener('click' , closemodal)
